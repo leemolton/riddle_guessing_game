6241aa6e1d877e91cdd2ef140c4f746333e84fcf
Lee Molton <leemolton@hotmail.co.uk>
2018-09-02 15:04:22 +0000

    Modified answering of riddle questions

--- quiz/data/test_riddle.py
+++ quiz/data/test_riddle.py
@@ -1,3 +1,25 @@
 import unittest
+import unittest.mock
 import quiz
+
+
+#Test suite
+class test_run(unittest.TestCase):
+
+#Basic functionality test
+    def test_is_this_thing_on(self):
+        self.assertEqual(1,1)
+
+print("All the tests passed!")
+
+#Test opening a file and writing data to it
+# def test_can_we_add_data
+
+
+#Test to check that data list is empty to start with
+#def test_is_list_empty
+
+
+
+
 
